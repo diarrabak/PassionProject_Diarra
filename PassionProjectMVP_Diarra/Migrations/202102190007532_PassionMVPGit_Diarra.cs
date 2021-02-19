6ff5f513775383aabdca3e1b805f032f6bd342e7
diarrabak <bakary.diarra9@gmail.com>
2021-02-18 19:38:38 -0500

    Second commit

---
+++ PassionProjectMVP_Diarra/Migrations/202102190007532_PassionMVPGit_Diarra.cs
@@ -0,0 +1,16 @@
+namespace PassionProjectMVP_Diarra.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PassionMVPGit_Diarra : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
